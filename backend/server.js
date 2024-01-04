c22117f9765159f4fd4a26b7e799a1bbbbb4ac16
unpairedkrish-7 <krishnabhukya893@gmail.com>
2024-01-04 12:19:38 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const connectedDb = require("./config/connectedDb");
+const Task = require("./model/taskModel.js")
+const taskRoutes = require("./routes/taskRouter.js")
+const cors = require("cors")
+
+
+const app = express();
+
+
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+app.use(cors({
+    origin : ["http://localhost:3000" , "https://mern-task-app.onrender.com"]
+}))
+
+
+app.get("/" , async (req,res)=>{
+    res.status(200).send("Home Page");
+})
+
+app.use("/api/tasks" , taskRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+
+
+const startServer = async()=>{
+    try {
+        await connectedDb()
+        app.listen(PORT , ()=>{
+            console.log(`Server is running on port ${PORT}`)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+startServer();
